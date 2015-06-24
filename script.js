3853101ebef9ce68a973268d6fe907ed03977ed8
The-Whisperer <alex.erixon@hotmail.se>
2015-06-24 09:12:22 +0200

    DLC

--- script.js
+++ script.js
@@ -3,7 +3,17 @@ var feeling = function(){
 	if (answer === "great") {
 		document.getElementById("demo").innerHTML = "Greattastic"
 	};
+	
 	if (answer === "date") {
 		document.getElementById("demo").innerHTML = Date();
 	};
+	
+	if (answer === "DLC") {
+		var div = document.getElementById("demo");
+		var img = document.createElement('img');
+
+		img.src = 'http://www.gametrailers.com/side-mission/files/2012/05/ea_money.jpg';
+  		div.appendChild(img);
+	}
+
 };
